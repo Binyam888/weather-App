f17f49299990660140d08bd8e6f13c66536649f4
mohammed binyamin <147620229+Binyam888@users.noreply.github.com>
2023-11-03 16:31:07 +0530

    Update script.js
    small change

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("heloo")
+
 const spi = "7f25580e5be5ae1c900766320379432d"
 const API_URL = "https://api.openweathermap.org/data/2.5/weather?q=germany"
 const api ="https://api.openweathermap.org/data/2.5/weather?units=metric&q="
