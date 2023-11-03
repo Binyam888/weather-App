7098a1f7c35a1febbccdc8888a7befe6e3c4331f
mohammed binyamin <binyam0068@gmail.com>
2023-11-03 15:13:11 +0530

    dynamicaly changed the values and dynamically changed the weather icon with if case depend apon the wether condition

--- script.js
+++ script.js
@@ -2,18 +2,52 @@ console.log("heloo")
 const spi = "7f25580e5be5ae1c900766320379432d"
 const API_URL = "https://api.openweathermap.org/data/2.5/weather?q=germany"
 const api ="https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+
+
+
 const search = document.getElementById('search-box')
 const button = document.getElementById('butn')
 const cityName = document.querySelector(".city-name")
 const temp = document.querySelector(".weather h1")
+const humidity = document.querySelector(".humdity")
+const windSpeed = document.querySelector(".wind-speed")
+const wetherIcon = document.querySelector(".weather-icon")
 
 
 async function weather (city){
     const response = await fetch(api + city + `&appid=${spi}`)
     const  data = await response.json()
-    console.log(data)
+    console.log(data.weather[0])
     cityName.innerHTML=data.name
-    temp.innerHTML= Math.round(data.main.temp)   
+    temp.innerHTML= Math.round(data.main.temp) + "°C"
+       windSpeed.innerHTML = data.wind.speed + "km/hr"
+       humidity.innerHTML = data.main.humidity +"%"
+        if(data.weather[0].main == "Clouds"){
+       wetherIcon.src ="pic/clouds.png"
+     }
+     else if (data.weather[0].main == "Clear"){
+        wetherIcon.src ="pic/clear.png"
+     }
+     else if (data.weather[0].main == "Snow")
+     {
+        wetherIcon.src ="pic/snow.png"
+
+     }
+     else if (data.weather[0].main == "Rain")
+     {
+        wetherIcon.src ="pic/rain.png"
+
+     }
+     else if (data.weather[0].main == "Drizzle")
+     {
+        wetherIcon.src ="pic/drizzle.png"
+
+     }
+     else if (data.weather[0].main == "Mist")
+     {
+        wetherIcon.src ="pic/mist.png"
+
+     }
 }
 
 button.addEventListener("click",()=>{
