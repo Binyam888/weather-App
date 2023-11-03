f8731ec7fc8db5f5c239959a4bac9216bc9cb647
mohammed binyamin <binyam0068@gmail.com>
2023-11-03 14:14:32 +0530

    api added ,api key assigned

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("heloo")
+const spi = "7f25580e5be5ae1c900766320379432d"
+const API_URL = "https://api.openweathermap.org/data/2.5/weather?q=germany"
+const api ="https://api.openweathermap.org/data/2.5/weather?q=germany&appid="
+
+
+async function weather (){
+    const response = await fetch(api + spi)
+    const  data = await response.json()
+    console.log(data)
+}
+weather()
